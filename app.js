6de0505747678c9e4890f373842f4726a116dd40
Istine <imanchesebastian@gmail.com>
2020-12-07 13:46:12 +0100

    added routes, model, controlllers and middleare directories

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const app = express() // call express function
 const PORT = process.env.PORT || 3000
 
 
-app.get("/", (req, res) => {
-    res.json({
-        message:"Hello sean!"
-    })
-})
-
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+//routes
+app.use("/", require("./routes/signup")) // singup route
+app.use("/login", require("./routes/login")) //login route
+app.use("/channels", require("./routes/channels")) // channels route
+
 // server listening on port 
-app.listen(PORT,() => console.log("Sever running on port" + PORT))
+app.listen(PORT,() => console.log("Sever running on port " + PORT))
 
---
+++ middleware/signup.middleware.js
@@ -0,0 +1,10 @@
+//middleware function for creating a new user
+const create_new_user = (req, res, next) => {
+    try {
+        const { username, password} =  req.body // get username and password from request body
+        
+        
+    } catch (error) {
+        
+    }
+}
