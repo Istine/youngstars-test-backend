41b13f72607d4319183b4da479da2ba2f9dd520c
Istine <imanchesebastian@gmail.com>
2020-12-07 12:43:12 +0100

    added express and nodemon

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express() // call express function
+
+const PORT = process.env.PORT || 3000
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message:"Hello World!"
+    })
+})
+
+
+// server listening on port 
+app.listen(PORT,() => console.log("Sever running on port" + PORT))
+
