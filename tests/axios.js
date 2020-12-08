7d02e2a9904461cde1a326862b1595b93281a3fd
Istine <imanchesebastian@gmail.com>
2020-12-08 12:37:58 +0100

    added description to mock axios

--- tests/axios.js
+++ tests/axios.js
@@ -1,4 +1,8 @@
-// mocking axios post function to be overriden
+// mocking axios put function to be overriden
 module.exports = {
     put: jest.fn((url, body, config) => Promise.resolve({}))
 }
+
+/**
+ * This is a mock axios put request to enable mocking of a particular request using jest
+ */
